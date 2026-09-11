8067b90b8172cad7f4665e3bd1163c5c3d9840e0
lu-perhaps <luciana.alvarez@udea.edu.co>
2026-09-11 08:37:09 -0500

    Initial Spring Boot project

---
+++ src/main/java/com/udea/bancodigital/BancoDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.udea.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/bancodigital/BancoDigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
