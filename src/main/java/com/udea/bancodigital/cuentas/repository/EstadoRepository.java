fa0c10d4014babfd497c3330ca535239d8f2f005
Cristian Sánchez <cristian.sanchez3@udea.edu.co>
2026-09-22 00:55:29 -0500

    Fix BeanDefinitionOverrideException by specifying distinct repository bean names

--- src/main/java/com/udea/bancodigital/cuentas/repository/EstadoRepository.java
+++ src/main/java/com/udea/bancodigital/cuentas/repository/EstadoRepository.java
@@ -2,8 +2,11 @@ package com.udea.bancodigital.cuentas.repository;
 
 import com.udea.bancodigital.cuentas.entity.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository("cuentasEstadoRepository")
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
     Optional<Estado> findByNombreIgnoreCase(String nombre);
 }
--- src/main/java/com/udea/bancodigital/usuarios/repository/EstadoRepository.java
+++ src/main/java/com/udea/bancodigital/usuarios/repository/EstadoRepository.java
@@ -2,8 +2,11 @@ package com.udea.bancodigital.usuarios.repository;
 
 import com.udea.bancodigital.usuarios.entity.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository("usuariosEstadoRepository")
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
     Optional<Estado> findByNombreIgnoreCase(String nombre);
 }
